f259ef7064961a63da2e35308b17e075d4560861
DWVoid <yshliu0321@icloud.com>
2025-10-20 19:41:31 +0800

    Parser VM

--- Nova/Compile/Parse.cs
+++ Nova/Compile/Parse.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System.Collections.Immutable;
+using System.Globalization;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Nova;
@@ -101,77 +103,11 @@ public static class Parse
         }
     }
 
-    public interface IRule
-    {
-        object? Try(ref Source.Cursor c);
-    }
 
     public interface IScan
     {
         bool Visit(in Grapheme g);
         bool Complete();
-    }
-
-    private sealed class ScanRule<TS>(TS init) : IRule where TS : struct, IScan
-    {
-        public object? Try(ref Source.Cursor c)
-        {
-            var cc = c;
-            var scan = init;
-            while (scan.Visit(cc.Current))
-                if (cc.MoveNext())
-                    break;
-            if (!scan.Complete()) return null;
-            var r = c.Slice(in cc);
-            c = cc;
-            return r;
-        }
-    }
-
-    private struct ExactScan(string s) : IScan
-    {
-        private int _match = 0;
-
-        public bool Visit(in Grapheme g)
-        {
-            var span = s.AsSpan(_match);
-            // guard against partial grapheme match
-            if (span.Length < g.Chars.Length) return false;
-            span = span[..g.Chars.Length];
-            // do sequence compare on full cluster length
-            var r = span.SequenceEqual(g.Chars);
-            if (r) _match += g.Chars.Length;
-            return r;
-        }
-
-        public bool Complete() => _match == s.Length;
-    }
-
-    private sealed class RepeatRule : IRule
-    {
-        public required IRule Rule { get; init; }
-        public required int Min { get; init; }
-        public required int Max { get; init; }
-
-        public object? Try(ref Source.Cursor c)
-        {
-            var cc = c;
-            var rr = new List<object>();
-            for (var i = 0; i < Max; i++)
-            {
-                var r = Rule.Try(ref cc);
-                if (r == null)
-                    if (i >= Min) break;
-                    else return null;
-                rr.Add(r);
-            }
-
-            return rr.ToArray();
-        }
-    }
-
-    public sealed class ParseContext
-    {
     }
 
     public interface ISelectionBuilder
@@ -343,7 +279,312 @@ public static class Parse
         public ISelectionBuilder Selection(string name) => new CSelB(this, name);
 
         public ISequenceBuilder Sequence(string name) => new CSeqB(this, name);
-        
+
+        private interface IRule
+        {
+            object? Try(ref Source.Cursor c);
+        }
+
+        private struct Frame
+        {
+            // 0 - selection
+            // 1 - sequential non anchored
+            // 2 - sequential anchored
+            // 3 - array builder
+            public int Mode;
+
+            // index to the name of current rule
+            public int Name;
+
+            // PC + 1 of what called this rule
+            public int Last;
+
+            // How the current value should be handled
+            // 0 - drop
+            // 1 - keep
+            // 2 - keep placeholder (mode array builder on drop)
+            public int Keep;
+        }
+
+        private sealed class Context
+        {
+            public readonly List<Frame> Steps = [];
+
+            public readonly List<object> Nodes = [];
+
+            public void PopFrame() => Steps.RemoveAt(Steps.Count - 1);
+
+            public ref Frame TopFrame() => ref CollectionsMarshal.AsSpan(Steps)[^1];
+        }
+
+        /*
+         * Seq:
+         * MODE 1
+         * NAME 1
+         * KEEP 0
+         * CALL 1
+         * FAIL
+         * MODE 2
+         * KEEP 1
+         * CALL 2
+         * FAIL
+         * BUILD 1
+         * DONE
+         */
+
+        /*
+         * Sel:
+         * MODE 0
+         * NAME 1
+         * CALL 1
+         * DONE
+         * CALL 2
+         * DONE
+         * CALL 3
+         * DONE
+         * FAIL
+         */
+
+        /*
+         * Arr:
+         * MODE 3
+         * CALL 1 CONST 0 CONST 0x7FFFFFF
+         * FAIL
+         * PACK
+         * DONE
+         */
+
+        /*
+         * Scan, does not use mode as it does not need call
+         * SCAN 1
+         * FAIL
+         * DONE
+         */
+
+        /*
+         * Stub:
+         * MODE 2
+         * CALL 1
+         * FAIL
+         * HALT
+         */
+
+        private enum Op
+        {
+            // set mode of current rule
+            Mode,
+
+            // set keep of current rule
+            Keep,
+
+            // set index of name of current rule
+            Name,
+
+            // setup call to use a rule by using its instruction offset as argument
+            // the following effects happens based on mode on Done or Fail is hit in the rule
+            // mode 0: do no extra on success, skip one instruction on fail
+            // mode 1 & 2: skip one instruction on success, do no extra on fail
+            // mode 3:
+            // on success, check second trailing constant and see if maximum is satisfied
+            // if so, return and skip one instruction, else repeat the call
+            // on failure, check first trailing constant and see if minimum is satisfied
+            // if so, return and skip one instruction, else return and do no extra
+            Call,
+
+            // use a scan rule of using its rule offset as argument
+            // on success, unconditionally skips an exception
+            Scan,
+
+            // pack the current frame into an array return
+            Pack,
+
+            // build the current frame into an object by calling a mapping rule by index
+            Build,
+
+            // mark the successful end of a sequence
+            Done,
+
+            // mark the failure end of a sequence
+            Fail,
+
+            // mark the end of evaluation and return the last element on stack
+            Halt,
+
+            // placeholder for inline extra const value
+            Const
+        }
+
+        private readonly record struct Ins(Op Op, int Arg);
+
+        private delegate bool Scan(Context c, ref Source.Cursor s);
+
+        private static readonly object PlaceHolder = new();
+
+        private sealed class Parser(
+            Ins[] ins,
+            Scan[] scan,
+            string[] name,
+            Func<ReadOnlySpan<object>, object>[] build,
+            Dictionary<string, int> symbols
+        )
+        {
+            private readonly string[] _name = name;
+
+            public object Run(string sym, Source src) => Run(symbols[sym], src);
+
+            private object Run(int pc, Source src)
+            {
+                var ctx = new Context();
+                var cur = new Source.Cursor(src, 0);
+                ctx.Steps.Add(new Frame());
+                ref var fra = ref ctx.TopFrame();
+                for (;; ++pc)
+                {
+                    var (op, arg) = ins[pc];
+                    switch (op)
+                    {
+                        case Op.Mode:
+                            fra.Mode = arg;
+                            if (arg == 3 && fra.Keep is 0 or 2) fra.Keep = 2;
+                            break;
+                        case Op.Keep:
+                            if (fra.Keep != 2) fra.Keep = arg;
+                            break;
+                        case Op.Name:
+                            fra.Name = arg;
+                            break;
+                        case Op.Call:
+                            ctx.Steps.Add(new Frame { Last = pc + 1, Keep = fra.Keep });
+                            fra = ref ctx.TopFrame();
+                            pc = arg;
+                            continue;
+                        case Op.Scan:
+                            if (scan[arg](ctx, ref cur)) ++pc;
+                            break;
+                        case Op.Pack:
+                            // handle the value keep rule
+                            switch (fra.Keep)
+                            {
+                                case 0:
+                                    ctx.Nodes.RemoveRange(fra.Last, ctx.Nodes.Count - fra.Last);
+                                    break;
+                                case 1:
+                                {
+                                    var o = ImmutableArray.Create(CollectionsMarshal.AsSpan(ctx.Nodes)[fra.Last..]);
+                                    ctx.Nodes.RemoveRange(fra.Last, ctx.Nodes.Count - fra.Last);
+                                    ctx.Nodes.Add(o);
+                                    break;
+                                }
+                                case 2:
+                                    ctx.Nodes.RemoveRange(fra.Last, ctx.Nodes.Count - fra.Last);
+                                    ctx.Nodes.Add(PlaceHolder);
+                                    break;
+                            }
+
+                            break;
+                        case Op.Build:
+                            // handle the value keep rule
+                            switch (fra.Keep)
+                            {
+                                case 0:
+                                    ctx.Nodes.RemoveRange(fra.Last, ctx.Nodes.Count - fra.Last);
+                                    break;
+                                case 1:
+                                {
+                                    var o = build[arg](CollectionsMarshal.AsSpan(ctx.Nodes)[fra.Last..]);
+                                    ctx.Nodes.RemoveRange(fra.Last, ctx.Nodes.Count - fra.Last);
+                                    ctx.Nodes.Add(o);
+                                    break;
+                                }
+                                case 2:
+                                    ctx.Nodes.RemoveRange(fra.Last, ctx.Nodes.Count - fra.Last);
+                                    ctx.Nodes.Add(PlaceHolder);
+                                    break;
+                            }
+
+                            break;
+                        case Op.Done:
+                        {
+                            pc = fra.Last;
+                            ctx.PopFrame();
+                            fra = ref ctx.TopFrame();
+                            switch (fra.Mode)
+                            {
+                                case 0:
+                                    continue;
+                                case 1:
+                                case 2:
+                                    ++pc;
+                                    continue;
+                                case 3:
+                                {
+                                    var max = ins[pc + 2].Arg;
+                                    var cnt = ctx.Nodes.Count - fra.Last;
+                                    if (cnt < max) --pc;
+                                    else ++pc;
+                                    continue;
+                                }
+                                default:
+                                    throw new InvalidOperationException();
+                            }
+                        }
+                        case Op.Fail:
+                        {
+                            if (fra.Mode == 2)
+                            {
+                                // TODO: emit diagnostics
+                                throw new Exception("Bad Grammar");
+                            }
+
+                            pc = fra.Last;
+                            ctx.PopFrame();
+                            fra = ref ctx.TopFrame();
+                            switch (fra.Mode)
+                            {
+                                case 0:
+                                    ++pc;
+                                    continue;
+                                case 1:
+                                case 2:
+                                    continue;
+                                case 3:
+                                {
+                                    var min = ins[pc + 1].Arg;
+                                    var cnt = ctx.Nodes.Count - fra.Last;
+                                    if (cnt >= min) ++pc;
+                                    continue;
+                                }
+                                default:
+                                    throw new InvalidOperationException();
+                            }
+                        }
+                        case Op.Halt:
+                            return ctx.Nodes[0];
+                        case Op.Const:
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+        }
+
+
+        private sealed class ScanRule<TS>(TS init) : IRule where TS : struct, IScan
+        {
+            public object? Try(ref Source.Cursor c)
+            {
+                var cc = c;
+                var scan = init;
+                while (scan.Visit(cc.Current))
+                    if (cc.MoveNext())
+                        break;
+                if (!scan.Complete()) return null;
+                var r = c.Slice(in cc);
+                c = cc;
+                return r;
+            }
+        }
+
         public void Build()
         {
             // find unresolved rule names
@@ -352,6 +593,7 @@ public static class Parse
                     Console.WriteLine($"Rule demanded but unresolved: {n}");
             Console.WriteLine($"Found {_named.Count} named rules");
             Console.WriteLine($"Found {_const.Count} const rules");
+            // 
         }
     }
 
